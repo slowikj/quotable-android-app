4b97d3033b61ea37d7302996c1f6a689cd002151
Jadwiga Słowik <jadwiga.slowik5@gmail.com>
2021-11-24 11:36:04 +0100

    Refactor: optimize imports

--- app/src/main/java/com/example/quotableapp/viewmodel/QuotesViewModel.kt
+++ app/src/main/java/com/example/quotableapp/viewmodel/QuotesViewModel.kt
@@ -5,8 +5,8 @@ import androidx.lifecycle.viewModelScope
 import androidx.paging.ExperimentalPagingApi
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
-import com.example.quotableapp.data.model.Quote
 import com.example.quotableapp.data.QuotesRepository
+import com.example.quotableapp.data.model.Quote
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
