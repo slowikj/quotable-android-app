4b1f217cbe1943d8d142cda2565cdadb506162bb
Jadwiga Słowik <jadwiga.slowik5@gmail.com>
2021-12-06 17:10:44 +0100

    OneQuoteFragment: add state/action architecture skeleton

--- app/src/main/java/com/example/quotableapp/view/onequote/OneQuoteFragment.kt
+++ app/src/main/java/com/example/quotableapp/view/onequote/OneQuoteFragment.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.quotableapp.databinding.OneQuoteFragmentBinding
 import com.example.quotableapp.view.common.rvAdapters.TagsAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
+
 @AndroidEntryPoint
 class OneQuoteFragment : Fragment() {
+
+    object StateHandler {
+
+        fun getDataOrNull(state: OneQuoteViewModel.State?): OneQuoteViewModel.State.Data? =
+            state as? OneQuoteViewModel.State.Data
+    }
 
     private val viewModel: OneQuoteViewModel by viewModels()
 
@@ -26,16 +35,40 @@ class OneQuoteFragment : Fragment() {
         binding = OneQuoteFragmentBinding.inflate(inflater).apply {
             viewModel = this@OneQuoteFragment.viewModel
             lifecycleOwner = this@OneQuoteFragment.viewLifecycleOwner
+            stateHandler = StateHandler
         }
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.rvTags.adapter = tagsAdapter
-        viewModel.quote.observe(viewLifecycleOwner) {
-            binding.letterIcon.letter = if (it.author.isNotEmpty()) it.author[0].toString() else "?"
-            tagsAdapter.submitList(it.tags)
+        binding.quoteLayout.rvTags.adapter = tagsAdapter
+        viewModel.state.observe(viewLifecycleOwner) { handle(it) }
+        viewModel.action.observe(viewLifecycleOwner) { handle(it) }
+    }
+
+    private fun handle(state: OneQuoteViewModel.State) {
+        // TODO
+        when (state) {
+            is OneQuoteViewModel.State.Data -> handleValidData(state)
         }
+    }
+
+    private fun handle(action: OneQuoteViewModel.Action) {
+        // TODO
+        when (action) {
+            is OneQuoteViewModel.Action.ShowError -> showErrorToast()
+        }
+    }
+
+    private fun handleValidData(state: OneQuoteViewModel.State.Data) {
+        binding.quoteLayout.letterIcon.letter =
+            if (state.quote.author.isNotEmpty()) state.quote.author[0].toString() else "?"
+        tagsAdapter.submitList(state.quote.tags)
+    }
+
+    private fun showErrorToast() {
+        Toast.makeText(context, "Error occurred", Toast.LENGTH_SHORT).show()
+        findNavController().popBackStack()
     }
 }
--- app/src/main/java/com/example/quotableapp/view/onequote/OneQuoteViewModel.kt
+++ app/src/main/java/com/example/quotableapp/view/onequote/OneQuoteViewModel.kt
@@ -3,6 +3,8 @@ package com.example.quotableapp.view.onequote
 import androidx.lifecycle.*
 import com.example.quotableapp.data.model.Quote
 import com.example.quotableapp.data.repository.OneQuoteRepository
+import com.example.quotableapp.view.common.MutableSingleLiveEvent
+import com.example.quotableapp.view.common.SingleLiveEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -13,18 +15,65 @@ class OneQuoteViewModel @Inject constructor(
     private val oneQuoteRepository: OneQuoteRepository
 ) : ViewModel() {
 
+    sealed class State {
+        object Error : State()
+
+        object Loading : State()
+
+        data class Data(val quote: Quote) : State()
+    }
+
+    sealed class Action {
+        sealed class Navigation : Action() {
+            data class ToAuthorQuotes(val author: String) : Action()
+
+            data class ToTagQuotes(val tag: String) : Action()
+        }
+
+        object ShowError : Action()
+    }
+
     companion object {
         const val QUOTE_ID = "quoteId"
     }
 
     private val quoteId: String = savedStateHandle[QUOTE_ID]!!
 
-    private val _quote: MutableLiveData<Quote> = MutableLiveData()
-    val quote: LiveData<Quote> = _quote
+    private val _state: MutableLiveData<State> = MutableLiveData()
+    val state: LiveData<State> = _state
+
+    private val _action: MutableSingleLiveEvent<Action> = MutableSingleLiveEvent()
+    val action: SingleLiveEvent<Action> = _action
 
     init {
+        onRefresh()
+    }
+
+    fun onRefresh() {
+        _state.postValue(State.Loading)
         viewModelScope.launch {
-            _quote.postValue(oneQuoteRepository.fetchQuote(quoteId))
+            runCatching { oneQuoteRepository.fetchQuote(quoteId) }
+                .onSuccess { _state.postValue(State.Data(it)) }
+                .onFailure {
+                    _state.postValue(State.Error)
+                    _action.postValue(Action.ShowError)
+                }
         }
+    }
+
+    fun onAuthorClick() {
+        // TODO
+    }
+
+    fun onTagClick(tag: String) {
+        // TODO
+    }
+
+    fun onLike() {
+        // TODO
+    }
+
+    fun onCopyToClipboard() {
+        // TODO
     }
 }
