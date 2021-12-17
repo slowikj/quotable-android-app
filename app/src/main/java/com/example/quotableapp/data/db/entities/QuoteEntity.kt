733714e4ac92907d57987628bfc621e9c8e72d44
Jadwiga Słowik <jadwiga.slowik5@gmail.com>
2021-12-17 17:56:33 +0100

    QuoteEntity: remove default values

--- app/src/main/java/com/example/quotableapp/data/db/entities/QuoteEntity.kt
+++ app/src/main/java/com/example/quotableapp/data/db/entities/QuoteEntity.kt
@@ -5,11 +5,11 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "quotes")
 data class QuoteEntity(
-    @PrimaryKey val id: String = "",
-    val content: String = "",
-    val author: String = "",
-    val authorSlug: String = "",
-    val tags: List<String> = emptyList(),
+    @PrimaryKey val id: String,
+    val content: String,
+    val author: String,
+    val authorSlug: String,
+    val tags: List<String>,
     val lastUpdated: Long = System.currentTimeMillis()
 ) {
 }
