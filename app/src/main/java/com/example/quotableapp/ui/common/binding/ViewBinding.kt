995ecc8b9670980bccfe13f5ac8d57ba0e544bff
Jadwiga Słowik <jadwiga.slowik5@gmail.com>
2022-02-23 11:29:31 +0100

    Use placeholders for images in AuthorListFragment
    Fix #9

--- app/src/main/java/com/example/quotableapp/ui/common/binding/ViewBinding.kt
+++ app/src/main/java/com/example/quotableapp/ui/common/binding/ViewBinding.kt
@@ -4,8 +4,18 @@ import android.widget.TextView
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.example.quotableapp.R
 
 object ViewBinding {
+
+    @JvmStatic
+    @BindingAdapter("loadImageWithPlaceholder")
+    fun bindLoadImageWithPlaceholder(view: AppCompatImageView, url: String?) {
+        Glide.with(view.context)
+            .load(url)
+            .placeholder(R.drawable.ic_person)
+            .into(view)
+    }
 
     @JvmStatic
     @BindingAdapter("loadImage")
