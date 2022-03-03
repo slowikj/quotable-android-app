b5440327cbca0a12aa4e626de2858cd589e8511a
Jadwiga Słowik <jadwiga.slowik5@gmail.com>
2022-03-03 09:12:40 +0100

    Extensions for LoadDataHandlerLineBinding: make filename compatible with binding and clean up comments

--- app/src/main/java/com/example/quotableapp/ui/common/extensions/LoadDataHandlerExtensions.kt
+++ app/src/main/java/com/example/quotableapp/ui/common/extensions/LoadDataHandlerExtensions.kt
@@ -5,7 +5,7 @@ import com.example.quotableapp.databinding.LoadDataHandlerLineBinding
 import com.example.quotableapp.ui.common.UiState
 
 fun LoadDataHandlerLineBinding.handle(state: UiState<*, *>) {
-    tvError.isVisible = state.run { error != null && !isLoading && data == null } //state.error != null && !state.isLoading
-    btnRetry.isVisible = state.run { error != null && !isLoading && data == null } //state.error != null && !state.isLoading
+    tvError.isVisible = state.run { error != null && !isLoading && data == null }
+    btnRetry.isVisible = state.run { error != null && !isLoading && data == null }
     progressBar.isVisible = state.isLoading
 }
