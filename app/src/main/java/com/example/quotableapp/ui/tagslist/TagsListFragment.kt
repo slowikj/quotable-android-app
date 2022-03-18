8bdf6c86f36476381bd8d620b425bdb455a6d258
Jadwiga Słowik <jadwiga.slowik5@gmail.com>
2022-03-18 11:27:58 +0100

    TagsListFragment: hide error message if data is available

--- app/src/main/java/com/example/quotableapp/ui/tagslist/TagsListFragment.kt
+++ app/src/main/java/com/example/quotableapp/ui/tagslist/TagsListFragment.kt
@@ -14,8 +14,10 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.quotableapp.data.model.Tag
 import com.example.quotableapp.databinding.FragmentTagsListBinding
+import com.example.quotableapp.ui.common.extensions.handle
 import com.example.quotableapp.ui.common.extensions.isLandscapeMode
 import dagger.hilt.android.AndroidEntryPoint
+import jp.wasabeef.recyclerview.animators.SlideInUpAnimator
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
@@ -40,6 +42,7 @@ class TagsListFragment : Fragment() {
     ): View {
         binding = FragmentTagsListBinding.inflate(inflater, container, false).apply {
             rvTags.adapter = tagsAdapter
+            rvTags.itemAnimator = SlideInUpAnimator()
             rvTags.layoutManager = GridLayoutManager(
                 context,
                 if (requireContext().isLandscapeMode) ITEMS_SPAN_LANDSCAPE else ITEMS_SPAN_PORTRAIT
@@ -67,10 +70,8 @@ class TagsListFragment : Fragment() {
         viewModel.tags.collectLatest { state ->
             tagsAdapter.submitList(state.data)
             with(binding) {
-                dataLoadHandler.btnRetry.isVisible = state.error != null
-                dataLoadHandler.tvError.isVisible = state.error != null
+                binding.dataLoadHandler.handle(state)
                 rvTags.isVisible = state.data?.isNotEmpty() ?: false
-                dataLoadHandler.progressBar.isVisible = state.isLoading
             }
         }
     }
