dc1b8585e6307af374de44fb2fdd868cfe056754
Jadwiga Słowik <jadwiga.slowik5@gmail.com>
2022-03-22 18:21:09 +0100

    LoadDataHandlerExtensions: refactor: use constraint layout group to handle visibility

--- app/src/main/java/com/example/quotableapp/ui/common/extensions/LoadDataHandlerExtensions.kt
+++ app/src/main/java/com/example/quotableapp/ui/common/extensions/LoadDataHandlerExtensions.kt
@@ -5,7 +5,6 @@ import com.example.quotableapp.databinding.LoadDataHandlerLayoutBinding
 import com.example.quotableapp.ui.common.UiState
 
 fun LoadDataHandlerLayoutBinding.handle(state: UiState<*, *>) {
-    tvError.isVisible = state.run { error != null && !isLoading && data == null }
-    btnRetry.isVisible = state.run { error != null && !isLoading && data == null }
-    progressBar.isVisible = state.isLoading
+    errorHandler.isVisible = state.run { error != null && !isLoading && data == null }
+    progressBar.isVisible = state.run { isLoading }
 }
