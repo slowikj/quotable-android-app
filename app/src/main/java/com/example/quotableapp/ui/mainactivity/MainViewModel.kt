4418c84a52b85011f95d519c98a505f0d52a9490
Jadwiga Słowik <jadwiga.slowik5@gmail.com>
2022-04-03 17:09:20 +0200

    MainViewModel: remove duplicated api calls
    Those calls are done simultaneously in VM of the first fragment (dashboard)

--- app/src/main/java/com/example/quotableapp/ui/mainactivity/MainViewModel.kt
+++ app/src/main/java/com/example/quotableapp/ui/mainactivity/MainViewModel.kt
@@ -2,44 +2,23 @@ package com.example.quotableapp.ui.mainactivity
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.quotableapp.common.CoroutineDispatchers
-import com.example.quotableapp.data.repository.authors.AuthorsRepository
-import com.example.quotableapp.data.repository.quotes.QuotesRepository
-import com.example.quotableapp.data.repository.tags.TagsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(
-    private val quotesRepository: QuotesRepository,
-    private val authorsRepository: AuthorsRepository,
-    private val tagsRepository: TagsRepository,
-    private val coroutineDispatchers: CoroutineDispatchers
-) : ViewModel() {
+class MainViewModel @Inject constructor() : ViewModel() {
 
     private val _isLoading: MutableStateFlow<Boolean> = MutableStateFlow(true)
     val isLoading: StateFlow<Boolean> = _isLoading.asStateFlow()
 
     init {
         viewModelScope.launch {
-            withContext(coroutineDispatchers.IO) {
-                val deferredList = listOf(
-                    async { quotesRepository.updateExemplaryQuotes() },
-                    async { quotesRepository.updateRandomQuote() },
-                    async { authorsRepository.updateExemplaryAuthors() },
-                    async { tagsRepository.updateAllTags() }
-                )
-
-                deferredList.forEach {
-                    it.join()
-                }
-            }
+            delay(1000)
 
             _isLoading.value = false
         }
